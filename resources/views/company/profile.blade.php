4d53ad4a8fc81669d8fad54c246113f784c04206
arsa <arsenije018@gmail.com>
2017-10-05 16:06:39 +0200

    Company profile template css and html

--- resources/views/company/profile.blade.php
+++ resources/views/company/profile.blade.php
@@ -1,4 +1,68 @@
 @extends('layouts.master')
 @section('content')
-	
+	<div class="main_app_container">
+		<div class="company_profile_view_holder cf">
+			<div class="company_profile_view_side cf">
+				<div class="company_profile_view_side_logo">
+					<img src="http://booproweb.com/img/booproweb-logo2.png" alt="">
+				</div>
+				<div class="company_profile_view_side_info">
+					<div class="company_profile_view_jobs_title">
+						<p class="bold">Company info</p>
+					</div>
+					<p class="bold">Booproweb</p>
+					<p class="bold"><a href="">www.booproweb.com</a></p>
+					<p class=""><span class="bold">Location:</span> Serbia<span>, <span>Nis</span>, <span>Ante Andrejevic 9</span></p>
+					<p><span class="bold">Phone:</span> <span>018/590-21</span> <span>069/590-21</span></p>
+					<p><span class="bold">Employers:</span> <span>12</span></p>
+				</div>
+				<div class="company_profile_view_jobs">
+					<div class="company_profile_view_jobs_title">
+						<p class="bold">All jobs by <span>Booproweb</span></p>
+					</div>
+					<ul>
+						<li class="company_profile_view_jobs_item">
+							<a href="" class="company_profile_view_jobs_item_link">
+								<p class="bold">We are looking for backend web developer</p>
+								<p class=""><span>Serbia</span>, <span>Nis</span></p>
+							</a>
+						</li>
+						<li class="company_profile_view_jobs_item">
+							<a href="" class="company_profile_view_jobs_item_link">
+								<p class="bold">We are looking for backend web developer</p>
+								<p class=""><span>Serbia</span>, <span>Nis</span></p>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="company_profile_view_main">
+				<div class="company_profile_view_cover">
+					<img style="width: 100%;" src="https://scontent.fbeg4-1.fna.fbcdn.net/v/t31.0-8/19702672_1617343671610432_6250076273563742580_o.jpg?oh=cd246e4b52bd4d88c2054c4ce088c5a4&oe=5A8552D0" alt="">
+				</div>
+				<div class="company_profile_view_main_item">
+					<div class="company_profile_view_main_title"><p class="bold">About company</p></div>
+					<div class="company_profile_view_main_text">
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					</div>
+				</div>
+
+				<div class="company_profile_view_main_item">
+					<div class="company_profile_view_main_title"><p class="bold">Careers</p></div>
+					<div class="company_profile_view_main_text">
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
