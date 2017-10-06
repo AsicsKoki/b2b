537f6a4f46c2cacec271b9041621fcde5e8df52a
arsa <arsenije018@gmail.com>
2017-10-06 13:46:41 +0200

    jobs filter page

---
+++ resources/views/ad/allAds.blade.php
@@ -0,0 +1,180 @@
+@extends('layouts.master')
+@section('content')
+	<main class="main_app_container">
+		<div class="pagination">
+			<ul class="cf">
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-left" aria-hidden="true" style="margin-right: 10px;"></i>Previus</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">1</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">2</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">3</a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href="">Next<i class="fa fa-angle-right" aria-hidden="true" style="margin-left: 10px;"></i></a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+				</li>
+			</ul>
+		</div>
+
+		<div class="job_list_filter_holder">
+			<ul>
+				<li class="job_list_filter_item cf">
+
+					<div class="job_list_filter_item_left">
+
+						<small class="date">Today</small>
+						<h3 class="job_list_filter_item_title bold">
+							<a href="">Customer Support Consultant fluent in both German and English</a>
+						</h3>
+						
+						<div class="job_list_filter_item_info">
+							<p class="location"><span class="bold">Location:</span>Belgrade</p>
+							<p class="salary"><span class="bold">Salary:</span>from 2500 to 3500 BGN (Gross)</p>	
+						</div>
+
+					</div>
+
+					<div class="job_list_filter_item_right">
+						
+						<div class="job_list_filter_item_right_section">
+							<div class="job_list_filter_item_logo">
+								<img src="http://booproweb.com/img/booproweb-logo2.png" alt="">
+							</div>
+						</div>
+
+						<div class="job_list_filter_item_right_section">
+							<p class="job_list_filter_item_company bold">Booproweb</p>
+							<ul class="company_ads_list">
+								<li>
+									<a href=""><i class="fa fa-lightbulb-o" aria-hidden="true"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="fa fa-video-camera" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+
+					</div>	
+					
+				</li>
+
+				<li class="job_list_filter_item cf">
+
+					<div class="job_list_filter_item_left">
+
+						<small class="date">Today</small>
+						<h3 class="job_list_filter_item_title bold">
+							<a href="">Customer Support Consultant fluent in both German and English</a>
+						</h3>
+						
+						<div class="job_list_filter_item_info">
+							<p class="location"><span class="bold">Location:</span>Belgrade</p>
+							<p class="salary"><span class="bold">Salary:</span>from 2500 to 3500 BGN (Gross)</p>	
+						</div>
+
+					</div>
+
+					<div class="job_list_filter_item_right">
+						
+						<div class="job_list_filter_item_right_section">
+							<div class="job_list_filter_item_logo">
+								<img src="http://booproweb.com/img/booproweb-logo2.png" alt="">
+							</div>
+						</div>
+
+						<div class="job_list_filter_item_right_section">
+							<p class="job_list_filter_item_company bold">Booproweb</p>
+							<ul class="company_ads_list">
+								<li>
+									<a href=""><i class="fa fa-lightbulb-o" aria-hidden="true"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="fa fa-video-camera" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+
+					</div>	
+					
+				</li>
+
+				<li class="job_list_filter_item cf">
+
+					<div class="job_list_filter_item_left">
+
+						<small class="date">Today</small>
+						<h3 class="job_list_filter_item_title bold">
+							<a href="">Customer Support Consultant fluent in both German and English</a>
+						</h3>
+						
+						<div class="job_list_filter_item_info">
+							<p class="location"><span class="bold">Location:</span>Belgrade</p>
+							<p class="salary"><span class="bold">Salary:</span>from 2500 to 3500 BGN (Gross)</p>	
+						</div>
+
+					</div>
+
+					<div class="job_list_filter_item_right">
+						
+						<div class="job_list_filter_item_right_section">
+							<div class="job_list_filter_item_logo">
+								<img src="http://booproweb.com/img/booproweb-logo2.png" alt="">
+							</div>
+						</div>
+
+						<div class="job_list_filter_item_right_section">
+							<p class="job_list_filter_item_company bold">Booproweb</p>
+							<ul class="company_ads_list">
+								<li>
+									<a href=""><i class="fa fa-lightbulb-o" aria-hidden="true"></i></a>
+								</li>
+								<li>
+									<a href=""><i class="fa fa-video-camera" aria-hidden="true"></i></a>
+								</li>
+							</ul>
+						</div>
+
+					</div>	
+					
+				</li>
+			</ul>
+		</div>
+
+		<div class="pagination">
+			<ul class="cf">
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-double-left" aria-hidden="true"></i></a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-left" aria-hidden="true" style="margin-right: 10px;"></i>Previus</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">1</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">2</a>
+				</li>
+				<li class="pagination_page">
+					<a href="">3</a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href="">Next<i class="fa fa-angle-right" aria-hidden="true" style="margin-left: 10px;"></i></a>
+				</li>
+				<li class="pagination_fast_page">
+					<a href=""><i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+				</li>
+			</ul>
+		</div>
+	</main>
+@endsection
