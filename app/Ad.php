32834c9b9cb4ad2db4114777e37f1be40f9cd878
Konstantin <cpt.koki@gmail.com>
2017-10-06 15:25:06 +0200

    Added fields for ad table

--- app/Ad.php
+++ app/Ad.php
@@ -10,4 +10,30 @@ class Ad extends Model
     {
     	return $this->belongsTo('App\Company');
     }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'description',
+        'category',
+        'job_type',
+        'term',
+        'company_id',
+        'career_level',
+        'students'
+        'country' ,
+        'city',
+        'salary_type'
+        'salary_from',
+        'salary_to',
+        'foreign_languages',
+        'questionnaire_id',
+        'external_url',
+        'position',
+
+    ];
 }
--- database/migrations/2017_09_26_085704_create_ads_table.php
+++ database/migrations/2017_09_26_085704_create_ads_table.php
@@ -11,6 +11,22 @@ class CreateAdsTable extends Migration {
 			$table->increments('id');
 			$table->timestamps();
 			$table->softDeletes();
+			$table->string('description', 100);
+			$table->string('category', 100);
+			$table->string('job_type', 100);
+			$table->string('term', 100);
+			$table->string('company_id', 100);
+			$table->string('career_level', 100);
+			$table->string('students', 100);
+			$table->string('country', 100);
+			$table->string('city', 100);
+			$table->string('salary_type', 100);
+			$table->string('salary_from', 100);
+			$table->string('salary_to', 100);
+			$table->string('foreign_languages', 100);
+			$table->string('questionnaire_id', 100);
+			$table->string('external_url', 100);
+			$table->string('position', 100);
 		});
 	}
 
