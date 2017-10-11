b8a5c7b3cae75a124dc16208776ffb87bbb66a03
arsa <arsenije018@gmail.com>
2017-10-11 14:57:28 +0200

    Single job view HTML and CSS

--- resources/views/ad/ad.blade.php
+++ resources/views/ad/ad.blade.php
@@ -1,5 +1,109 @@
 @extends('layouts.master')
-
 @section('content')
+<div class="single_job_content cf">
 
+	<div class="single_job_header">
+		<h1 class="single_job_title bold">We are looking for frontend developer for work in office</h1>
+		<ul class="single_job_categories cf">
+			<li>
+				<span class="bold">Categories:</span>
+			</li>
+			<li>
+				<a href="">IT</a>
+			</li>
+			<li>
+				<a href="">Computer Ingenier</a>
+			</li>
+			<li>
+				<a href="">Programming</a>
+			</li>
+		</ul>
+	</div>
+	<div class="single_job_sidebar">
+		<div class="single_job_sidebar_item">
+			<small class="single_job_published_date" style="display: block;"><span class="bold">Published:</span> <span>2.2.2014.</span></small>
+			<div class="single_job_main_logo">
+				<a href="">
+					<img src="http://booproweb.com/img/booproweb-logo2.png" alt="">
+				</a>
+			</div>
+			<h3 class="single_job_sidebar_item_title company_name bold">Company:</h3>
+			<p>Booproweb</p>
+		</div>
+
+		<div class="single_job_sidebar_item">
+			<h3 class="single_job_sidebar_item_title bold">Job location:</h3>
+			<p><span>Belgrade</span>, <span>Serbia</span></p>
+		</div>
+
+		<div class="single_job_sidebar_item">
+			<h3 class="single_job_sidebar_item_title bold">Languages:</h3>
+			<p>English</p>
+		</div>
+
+		<div class="single_job_sidebar_item">
+			<h3 class="single_job_sidebar_item_title bold">Job type:</h3>
+			<p>Full time</p>
+		</div>
+
+		<div class="single_job_sidebar_item">
+			<h3 class="single_job_sidebar_item_title bold">Salary:</h3>
+			<p><span>Bruto</span>, from <span>200$</span> to <span>400$</span></p>
+		</div>
+
+		<div class="single_job_sidebar_item">
+			<h3 class="single_job_sidebar_item_title bold">Career level:</h3>
+			<p>Expert</p>
+		</div>
+	</div>
+	<main class="main_app_container single_job_main">
+
+		<div class="single_job_main_cover">
+			<img src="http://booproweb.com/img/ilya-pavlov-87438.jpg" alt="">
+		</div>
+
+		<div class="single_job_main_section single_job_main_section_first">
+			<p class="single_job_students">This job is available for: <span class="bold">Students</span></p>
+			<p class="single_job_students">Level of experience required: <span class="bold">Expert</span></p>
+		</div>
+
+		<div class="single_job_desc">
+			<h3 class="bold">Job description</h3>
+			<p>Support & Compliance Agent with English</p>
+			<p>Are you looking for a place to challenge and develop your skills? Are you ambitious to make large money on your own?</p>
+
+			<p>Come and join us today!</p>
+
+			<p>Who are we?</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+
+			<p>GADOL Ltd is a global company providing outsourcing services for Support, IT and Sales positions.
+			We are currently looking to expand our team in Bulgaria with young, dynamic and energetic individuals who are confident and hungry for money.
+			We offer a fantastic opportunity for anyone looking for personal development and a successful career in the online business.</p>
+		</div>
+	</main>
+</div>
 @endsection
