1b4d299f215388fab8da6e4fe411958f58b7364b
arsa <arsenije018@gmail.com>
2017-11-02 10:58:44 +0100

    Updated profile

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -54,8 +54,105 @@
 <<<<<<< Updated upstream
 						<input type="text" placeholder="Enter education" style="font-size: 14px" >
 						<button class="confirm_edit_btn blue_btn" id="education_btn">Confirm</button>
+=======
+						<input type="text" placeholder="Education" style="font-size: 14px">
+						<button class="confirm_edit_btn blue_btn">Confirm <i class="fa fa-check" aria-hidden="true"></i></button>
 					</div>
 	
+				</div>
+
+
+				<div class="user_profile_item user_skills_holder">
+					<div class="user_profile_skills">
+						<p class="bold" style="text-align: left;">My skills:</p>
+						<span>PHP, HTML, CSS, Project menager, profesor, auto mehanicar, etc...</span>
+						<a class="edit_link" id="user_skills">	
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</a>
+>>>>>>> Stashed changes
+					</div>
+
+					<div class="edit_info_window" id="user_skills" style="">
+						<textarea name="" cols="30" rows="10" width="80%" placeholder="Enter your skills" style="height: 50px;"></textarea>
+						<button class="confirm_edit_btn blue_btn">Confirm <i class="fa fa-check" aria-hidden="true"></i></button>
+					</div>
+				</div>
+
+				<div class="user_profile_item user_profile_languages_holder">
+					<p class="bold" style="text-align: left;font-size: 15px;position: relative;display: inline-block;">
+					<span>Languages:</span>
+					<a href="#" class="edit_link" id="">	
+						<i class="fa fa-pencil" aria-hidden="true"></i>
+					</a>
+					</p>
+					<ul class="user_profile_languages cf">
+						<li><span>English</span></li>
+						<li><span>Serbian</span></li>
+					</ul>
+					
+				</div>
+
+				<div class="user_profile_item user_profile_work_prev">
+		
+						<ul>
+							<li class="bold">
+
+								<span class="user_work_position">Frontend developer</span> 
+								
+								<span class="work_place_user">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span>Booproweb</span>
+									<i class="fa fa-circle" aria-hidden="true"></i>
+								</span>
+	
+								<span class="period_prev_work">2014 - 2017</span>
+
+								<a href="#" class="edit_link" id="user_skills">	
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</a>
+			
+							</li>
+
+							<li class="bold">
+
+								<span class="user_work_position">Frontend developer</span> 
+								
+								<span class="work_place_user">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span>Booproweb</span>
+									<i class="fa fa-circle" aria-hidden="true"></i>
+								</span>
+	
+								<span class="period_prev_work">2014 - 2017</span>
+
+								<a href="#" class="edit_link" id="user_skills">	
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</a>
+			
+							</li>
+
+							<li class="bold">
+
+								<span class="user_work_position">Frontend developer</span> 
+								
+								<span class="work_place_user">
+									<i class="fa fa-circle" aria-hidden="true"></i>
+									<span>Booproweb</span>
+									<i class="fa fa-circle" aria-hidden="true"></i>
+								</span>
+	
+								<span class="period_prev_work">2014 - 2017</span>
+
+								<a href="#" class="edit_link" id="user_skills">	
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</a>
+			
+							</li>
+						</ul>
+						
+						<div class="add_new_user_job_profile">
+							<button class="add_new_user_job_profile_btn"><i class="fa fa-plus" aria-hidden="true"></i> Add new job</button>
+						</div>
 				</div>
 
 				<div class="user_profile_about user_profile_item">
@@ -68,7 +165,11 @@
 					
 					<div class="edit_info_window" id="description_input" style="display:none">
 						<textarea name="" cols="30" rows="10" width="100%" placeholder="Say something about you"></textarea>
+<<<<<<< Updated upstream
 						<button class="confirm_edit_btn blue_btn" id="description_btn">Confirm</button>
+=======
+						<button class="confirm_edit_btn blue_btn">Confirm <i class="fa fa-check" aria-hidden="true"></i></button>
+>>>>>>> Stashed changes
 					</div>
 				</div>
 			</div>
@@ -172,10 +273,111 @@
 
 		</div>
 	</div>
+
+	<!-- Pop up add user language -->
+	<div class="popUp popup_new_language_user">
+		<form action="" method="" class="popup_new_language_user_form">
+			<div class="popup_new_language_user_item">
+				<p>Choose language <i class="fa fa-angle-down" aria-hidden="true"></i></p>
+				<ul class="new_language_user_list">
+					<li><a href="">English</a></li>
+					<li><a href="">Serbian</a></li>
+					<li><a href="">Spanish</a></li>
+					<li><a href="">Maxican</a></li>
+				</ul>
+				<button class="add_new_lang_btn"><i class="fa fa-plus" aria-hidden="true"></i></button>
+			</div>
+			<div class="popup_new_job_user_form_item" style="background: transparent;margin: 0;padding: 0;box-shadow: none;">
+				<input type="submit" value="Save" class="save_btn_pop_up">
+			</div>
+		</form>
+
+		<a href="#" class="popup_close">
+			<i class="fa fa-times" aria-hidden="true"></i>
+		</a>
+	</div>
+	<!-- End -->
+
+	<!-- Pop up add new history Job -->
+	<div class="popUp popup_new_job_user">
+		<form action="" method="" class="popup_new_job_user_form">
+			<div class="popup_new_job_user_form_item">
+				<p class="popup_new_job_user_form_item_title bold">Job position:</p>
+				<input type="text">
+			</div>
+
+			<div class="popup_new_job_user_form_item">
+				<p class="popup_new_job_user_form_item_title bold">Company:</p>
+				<input type="text" placeholder="Company name">
+				<input type="text" placeholder="Company website">
+			</div>
+
+			<div class="popup_new_job_user_form_item cf">
+
+				<div class="user_from_to_period">				
+					<p class="popup_new_job_user_form_item_title bold">From:</p>
+					<div class="user_from_to_period_item">
+						<select name="" id="">
+							<option value="">Choose year</option>
+							<option value="">2017</option>
+							<option value="">2016</option>
+							<option value="">2015</option>
+						</select>
+					</div>
+
+					<div class="user_from_to_period_item">
+						<select name="" id="">
+							<option value="">Choose month</option>
+							<option value="">Januar</option>
+							<option value="">Februar</option>
+							<option value="">March</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="user_from_to_period">
+					<p class="popup_new_job_user_form_item_title bold">To:</p>
+					<div class="user_from_to_period_item">
+						<select name="" id="">
+							<option value="">Choose year</option>
+							<option value="">2017</option>
+							<option value="">2016</option>
+							<option value="">2015</option>
+						</select>
+					</div>
+
+					<div class="user_from_to_period_item">
+						<select name="" id="">
+							<option value="">Choose month</option>
+							<option value="">Januar</option>
+							<option value="">Februar</option>
+							<option value="">March</option>
+						</select>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="popup_new_job_user_form_item">
+				<p class="popup_new_job_user_form_item_title bold">Description:</p>
+				<textarea name="" id="" cols="30" rows="10" "></textarea>
+			</div>
+
+			<div class="popup_new_job_user_form_item" style="background: transparent;margin: 0;padding: 0;box-shadow: none;">
+				<input type="submit" value="Save" class="save_btn_pop_up">
+			</div>
+		</form>
+		<a href="#" class="popup_close">
+			<i class="fa fa-times" aria-hidden="true"></i>
+		</a>
+	</div>
+	<!-- End -->
+
 </main>
 
 <script>
 
+<<<<<<< Updated upstream
 // avatar/ profile image   - nezavrsen
  
 	$('#avatar').click(function(){
@@ -209,6 +411,19 @@
 		           {
 		            console.log(data);
 		           })
+=======
+// // avatar/ profile image   - nezavrsen
+
+// 	$('#avatar').classick(function(){
+// 			$(this).css('display', 'none');
+// 			$('#avatar_input').slideToggle('slow');
+// 		});
+
+// 		$('#avatar_input button').click(function(e){
+// 			e.preventDefault();
+// 			$('#avatar').css('display','block');	
+// 		});
+>>>>>>> Stashed changes
 
 		});
 
