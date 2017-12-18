1e8bcf0d38575055b44a55c4ec9cfedbe3dee04d
arsa <arsenije018@gmail.com>
2017-12-18 16:53:12 +0100

    Reset pass template CSS HTML

---
+++ resources/views/user/resetPasswordForm.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+@section('content')
+<h1 class="page_title main_page_title">Reset password</h1>
+
+<main class="main_app_container">
+	<form action="" method="" class="form_reset_password">
+		<div class="form_reset_password_item">
+			<input type="password" placeholder="Enter new password">
+		</div>
+		<div class="form_reset_password_item">
+			<input type="password" placeholder="Confirm new password">
+		</div>
+
+		<div class="form_reset_password_item">
+			<input type="submit" value="Confirm">
+		</div>
+	</form>
+</main>
+@endsection	
