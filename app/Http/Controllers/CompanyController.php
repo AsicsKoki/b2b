9095b7225b3b3e39601d620b35fa521ed5663e88
Josif Kostić <joskekostic@gmail.com>
2017-12-23 15:53:28 +0100

    Fixed apply button visibility, submit button on payment blade, edit company business sector little fix(still have bug),reset and confirm mail blades fixed,user profile languages working(paorski)

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -120,6 +120,9 @@ class CompanyController extends Controller {
 
     public function postEditCompany(Request $request)
     {
+        // $categories = Company::where('id', Auth::user()->id)->pluck('sector');
+        // return array_map('intval', explode(',', $categories[0]));
+
         $company=Company::find($request->cid);
         $businessCard = $company->businessCard;
 
@@ -133,7 +136,10 @@ class CompanyController extends Controller {
         $businessCard->number_of_employees = $request->number_of_employees;
 
         //implode da se regulise
-        $company->sector = serialize($request->sectors);
+        //$company->sector = serialize($request->sectors);
+        if(isset($request->sectors))
+        $company->sector = implode(',', $request->sectors);
+        else $company->sector=0;
 
         $company->save();
         $businessCard->save();
